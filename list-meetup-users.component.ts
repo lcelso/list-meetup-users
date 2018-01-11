d1ec2f6fbdd3d2234f993ef117af92d18d1f0309
Luiz Celso de Faria Alves <luiz.alves@concrete.com.br>
2018-01-11 02:37:53 -0200

    Create component

---
+++ list-meetup-users.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit , AfterViewInit} from '@angular/core';
+
+@Component({
+  selector: 'app-list-meetup-users',
+  templateUrl: './list-meetup-users.component.html',
+  styleUrls: ['./list-meetup-users.component.scss']
+})
+export class ListMeetupUsersComponent implements OnInit {
+
+  @Input() data;
+
+  modalIsOpen = false;
+
+  item;
+
+  ngOnInit() {
+    console.log(this.data);
+  }
+
+  openModal(img) {
+    this.item = img;
+    this.modalIsOpen = true;
+  }
+
+}
