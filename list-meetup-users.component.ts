08b16d35b9e4a017e6222f87ff6c26e05c582e45
Luiz Celso de Faria Alves <luiz.alves@concrete.com.br>
2018-01-11 13:42:21 -0200

    Remove modal

--- list-meetup-users.component.ts
+++ list-meetup-users.component.ts
@@ -1,24 +1,13 @@
-import { Component, Input, OnInit , AfterViewInit} from '@angular/core';
+import { Component, Input, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-list-meetup-users',
   templateUrl: './list-meetup-users.component.html',
   styleUrls: ['./list-meetup-users.component.scss']
 })
-export class ListMeetupUsersComponent implements OnInit {
+export class ListMeetupUsersComponent {
 
   @Input() data;
 
-  modalIsOpen = false;
-
   item;
-
-  ngOnInit() {
-  }
-
-  openModal(img) {
-    this.item = img;
-    this.modalIsOpen = true;
-  }
-
 }
